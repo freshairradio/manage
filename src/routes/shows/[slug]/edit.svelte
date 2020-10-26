db97d21d7c8f22750a9522f4aba01265db301e9b
Samuel Macleod <samuel@macleod.space>
2020-10-26 11:45:49 +0000

    correct RSS

--- src/routes/shows/[slug]/edit.svelte
+++ src/routes/shows/[slug]/edit.svelte
@@ -267,9 +267,9 @@
               <div class="mt-1 flex rounded-md shadow-sm">
                 <a
                   target="__blank"
-                  href="https://podcastsconnect.apple.com/my-podcasts/new-feed?submitfeed=https://freshair.nyc3.digitaloceanspaces.com/rssfeed/${show.slug}.xml"
+                  href="https://podcastsconnect.apple.com/my-podcasts/new-feed?submitfeed=https://freshair.nyc3.digitaloceanspaces.com/rssfeed/{show.slug}.xml"
                   class="form-input flex-1 block w-full px-3 py-2 rounded-md
-              sm:text-sm sm:leading-5"
+              sm:text-sm sm:leading-5 bg-blue-500"
                   placeholder="3pm">Validate</a>
               </div>
             </div>
